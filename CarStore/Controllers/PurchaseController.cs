a268bad75f03bd3ecbd24e26be5d0180f2406db2
Roman <first.luker@gmail.com>
2020-03-12 11:54:34 +0200

    Added purchase controller and views.

---
+++ CarStore/Controllers/PurchaseController.cs
@@ -0,0 +1,126 @@
+﻿using CarStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarStore.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        private CarStoreContext db = new CarStoreContext();
+        // GET: Purchase
+        public ActionResult Index()
+        {
+           // Purchase result = null;
+
+            //if (result.Id == id)
+            //{
+            //    result = db.Purchases.FirstOrDefault(p => p.Id == id);
+                
+            //}
+            return View(db.Purchases.ToList());
+
+        }
+
+        // GET: Purchase/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = db.Purchases.Find(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchase);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Customer,Phone,Date,BrandId,CarModelId,ConfigId,CarColorId")] Purchase purchase)
+        {
+            if (ModelState.IsValid)
+            {
+                purchase.Date = DateTime.Now;
+                db.Purchases.Add(purchase);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(purchase);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = db.Purchases.Find(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchase);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Customer,Phone,Date,BrandId,CarModelId,ConfigId,CarColorId")] Purchase purchase)
+        {
+            if (ModelState.IsValid)
+            {
+                purchase.Date = DateTime.Now;
+                db.Entry(purchase).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(purchase);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = db.Purchases.Find(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchase);
+        }
+
+        // POST: Purchases/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Purchase purchase = db.Purchases.Find(id);
+            db.Purchases.Remove(purchase);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
